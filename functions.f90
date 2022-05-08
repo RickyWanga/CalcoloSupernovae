081983a218412dfe5572eafad7d58c93172d969a
Ricky Wanga <ricky.wanga@hotmail.com>
2022-05-08 19:11:06 +0200

    aggiunto controllo per le righe da non considerare

--- functions.f90
+++ functions.f90
@@ -1,12 +1,13 @@
 program main
     
     implicit none
-    real(8), dimension(10) :: x,y,z
+    real(8), dimension(10) :: x,y,z,controllo
     integer :: i
     integer :: FID = 3
     character*256 :: CTMP
     integer :: J = 0, IERR = 0, NUM_LINES = 0
 
+    controllo = 99.90000
     open(unit=3, file="test_riga.dat")
 
     do while (IERR == 0)
@@ -20,7 +21,9 @@ program main
     i = 1
     do i=1, NUM_LINES
         read(3, *) x(i), y(i), z(i)
-        print '(3(1x f0.5))', x(i), y(i), z(i)
+        if(y(i)<99) then
+            print '(3(1x f0.5))', x(i), y(i), z(i)
+        end if
     end do
     close(3)
 

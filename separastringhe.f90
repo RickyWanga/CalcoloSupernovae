77e853e133479d909df5d69918b95d4f8e6aad96
RickyWanga <ricky.wanga@hotmail.com>
2022-08-01 20:59:02 +0200

    Risolto lettura dati dal file con le osservazioni e le conversioni del tipo di dato da stringa a reale

--- separastringhe.f90
+++ separastringhe.f90
@@ -1,35 +1,44 @@
-program main
-    use stringifor_string_t
-       implicit none
-       type( string ) ::  string1
-       type( string ), allocatable :: substrings( : )
-       character*256, allocatable :: MY_ARRAY(:)
-        integer :: I = 0, IERR = 0, NUM_LINES = 0
-       integer :: i
-       open(unit=FID,file='SN_data/SN2004dt.dat')
+program readText
+  implicit none
+  ! Dichiaro gli array di tipo reale 
+  real(kind=8), allocatable :: first(:)
+  real(kind=8), allocatable :: second(:)
+  real(kind=8), allocatable :: third(:)
+  
+  integer :: FID = 1
+  character*256 :: CTMP
+  
+  integer :: I = 0, IERR = 0, NUM_LINES = 0
+  
+  open(unit=FID,file='test_riga.dat')
+  
+  ! 2. Conta le righe di dati/osservazioni presenti
+  do while (IERR == 0)
+    NUM_LINES = NUM_LINES + 1
+    read(FID,*,iostat=IERR) CTMP
+  end do
+  NUM_LINES = NUM_LINES - 1
+  write(*,'(A,I0)') "Number of lines = ", NUM_LINES
+  
+  ! 3. Alloca i vari array per memorizzare i dati
+  allocate(first(NUM_LINES),second(NUM_LINES),third(NUM_LINES))
+  
+  ! Legge i contenuti del file
+  rewind(FID)
+  do I = 1, NUM_LINES
+    read(FID,*) first(I), second(I), third(I)
+  end do
+  !Stampa i dati presi dal file
+  do I=1, NUM_LINES
+    print *, first(I), second(I), third(I)
+  end do
 
-       ! 2. Get number of lines
-       do while (IERR == 0)
-         NUM_LINES = NUM_LINES + 1
-         read(FID,*,iostat=IERR) CTMP
-       end do
-       NUM_LINES = NUM_LINES - 1
-       write(*,'(A,I0)') "Number of lines = ", NUM_LINES
-       
-       ! 3. Allocate array of strings
-       allocate(MY_ARRAY(NUM_LINES))
-       
-       ! 4. Read the file content
-       rewind(FID)
-       do I = 1, NUM_LINES
-         read(FID,'(A)') MY_ARRAY(I)
-       end do
-       
-       ! 5. Print array to standard output
-       do I = 6,size(MY_ARRAY,1)
-        call MY_ARRAY(I)%split(sep=' ', tokens=substrings)
-        do i=1,size(substrings, dim=1)
-            write(*,*) substrings(i)
-        enddo
-       end do
-    end program main
+  write(*,*) "Printing the funny part"
+  
+  ! Deallocazioe dei vari array e chiudo la stream con il file dati
+  deallocate(first, second, third)
+  close(FID)
+  
+end program readText
+  
+  

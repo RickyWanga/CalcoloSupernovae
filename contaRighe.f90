0ff0a5c8b238c78d17806ade7370ea53c51f38bc
RickyWanga <ricky.wanga@hotmail.com>
2022-08-06 00:58:58 +0200

    Optimized code

--- contaRighe.f90
+++ contaRighe.f90
@@ -1,7 +1,7 @@
 module ContaRighe
     implicit none
     ! Variabile per lo stream del file da aprire
-    integer :: FID = 1
+    integer :: FIDConta = 42
     ! Variabili per il conteggio dei file
     character*256 :: CTMP
     integer :: I = 0, IERR = 0, NUM_LINES = 0, numRighe = 0
@@ -13,15 +13,15 @@ integer function Conteggio(nomeFile)
     
     character(*), intent(in) :: nomeFile
 
-    open(unit=FID, file=nomeFile)
+    open(unit=FIDConta, file=nomeFile)
 
     do while (IERR == 0)
         NUM_LINES = NUM_LINES + 1
-        read(FID,*,iostat=IERR) CTMP
+        read(FIDConta,*,iostat=IERR) CTMP
     end do
     NUM_LINES = NUM_LINES - 1
 
-    close(FID)
+    close(FIDConta)
 
     Conteggio = NUM_LINES
     return
--- separastringhe.f90
+++ separastringhe.f90
@@ -5,35 +5,26 @@ program readText
   real(kind=8), allocatable :: first(:)
   real(kind=8), allocatable :: second(:)
   real(kind=8), allocatable :: third(:)
+  integer :: NUM_RIGHE = 0, FID = 1
+
+  NUM_RIGHE = Conteggio("test_riga.dat")
+  write(*,'(A,I0)') "Number of lines = ", NUM_RIGHE
   
-  integer :: FID = 1
-  character*256 :: CTMP
-  
-  integer :: I = 0, IERR = 0, NUM_LINES = 0, J =0
-  
-  open(unit=FID,file='test_riga.dat')
-  
-  ! 2. Conta le righe di dati/osservazioni presenti
-  do while (IERR == 0)
-    NUM_LINES = NUM_LINES + 1
-    read(FID,*,iostat=IERR) CTMP
-  end do
-  NUM_LINES = NUM_LINES - 1
-  write(*,'(A,I0)') "Number of lines = ", NUM_LINES
+  open(unit=FID, file="test_riga.dat")
   
   ! 3. Alloca i vari array per memorizzare i dati
-  allocate(first(NUM_LINES),second(NUM_LINES),third(NUM_LINES))
+  allocate(first(NUM_RIGHE),second(NUM_RIGHE),third(NUM_RIGHE))
   
   ! Legge i contenuti del file
   rewind(FID)
-  do I = 1, NUM_LINES
+  do I = 1, NUM_RIGHE
     read(FID,*) first(I), second(I), third(I)
   end do
   !Stampa i dati presi dal file sul terminale
   I = 1
   do
     ! Se I diventa uguale al numero di righe esce dal loop
-    if(I == NUM_LINES) then
+    if(I == NUM_RIGHE) then
       exit
     ! Controllo che i dati dell'osservazione siano validi
     else if (.not.((second(I) >= 99.0).and.(second(I)<= 99.99))) then

80f5bbeae3a1c7c8438ca5e02c847935e35e4c9b
RickyWanga <ricky.wanga@hotmail.com>
2022-08-16 20:49:29 +0200

    Aggiunto codice per controllare e generare file di dati puliti

--- CheckGenerate.f90
+++ CheckGenerate.f90
@@ -10,16 +10,34 @@ program CheckGenerate
     real(kind=8) :: x = 0, y = 0
     character*6, allocatable :: nomiSupernovae(:)
     character*20, allocatable :: FileNomiSupernovae(:)
-    character*24, allocatable :: PathSupernovae(:)
+    character*20, allocatable :: PathSupernovae(:)
     integer :: NUM_RIGHE = 0, F = 1, N = 1, NUM_SUP = 0, M = 0, Max = 0, Scartati = 0
     integer :: J = 0, Z = 0
 
     character*256 :: CTMP
     integer :: I = 0, IERR = 0, NUM_LINES = 0
 
-    do N = 10, 10
+    NUM_SUP = 70
+    allocate(nomiSupernovae(NUM_SUP), PathSupernovae(NUM_SUP), FileNomiSupernovae(NUM_SUP))
+
+    open(42,file="NomiFileSupernovae.dat")
+    do N = 1, NUM_SUP
+        read(42,*) nomiSupernovae(N)
+        FileNomiSupernovae(N) = 'SN_temp/SN' // (nomiSupernovae(N)) // '.dat'
+        PathSupernovae(N) = 'SN_data/SN' // (nomiSupernovae(N)) // '.dat'
+    end do
+    close(42)
+    
+    do I=1, 70
+      print *, PathSupernovae(I), FileNomiSupernovae(I)
+    end do
+
+
+
+    do N = 10, 79
         F = N - 9
-        open(unit=N, file="SN_data/SN2004dt.dat")
+        open(unit=N, file=PathSupernovae(F))
+        print *, "Nome : ", PathSupernovae(F)
         print *, "opened file no : ", F
         NUM_LINES = 0
         IERR = 0
@@ -46,54 +64,60 @@ program CheckGenerate
           end if
         end do
         close(N)
-        
+
         I = 1
         do
         ! Se I diventa uguale al numero di righe esce dal loop
           if(I > Max) then
             print *, "Fine File"
-            allocate(MJDtemp(Max-Scartati), BandaBtemp(Max-Scartati), e_BandaBtemp(Max-Scartati))
-            J = 1
-            Z = 1
-            print *, "Valori scartati : ", Scartati
-            do while(J<Max)
-                if (.not.((BandaB(J) >= 99.0).and.(BandaB(J)<= 99.99))) then
-                    MJDtemp(Z) = MJD(J)
-                    BandaBtemp(Z) = BandaB(J)
-                    e_BandaBtemp(Z) = e_BandaB(J)
-                    J = J + 1
-                    Z = Z + 1
-                    print *,"NumRiga, NumArray, NumArraytemp ",I, J, Z
-                else
-                    J = J + 1
-                    print *, "Vado avanti perche dato non valido"
-                end if
-            end do
-            print *,"STAMPO I DATI CORRETTI IN UN FILE DI PROVA"
-            open(99, file="filediprova.dat")
-                do I=1, Z-1
-                    write(99, *) MJDtemp(I), BandaBtemp(I), e_BandaBtemp(I)
-                end do
-            close(99)
-
-            deallocate(MJDtemp, BandaBtemp, e_BandaBtemp)
             exit
         ! Controllo che i dati dell'osservazione siano validi
           else if (.not.((BandaB(I) >= 99.0).and.(BandaB(I)<= 99.99))) then
-            print *, MJD(I), BandaB(I)
             I = I + 1
           else
-            print *, "Valore scartato"
             Scartati = Scartati + 1
             I = I + 1
           end if
         end do
         
+        allocate(MJDtemp(Max-Scartati), BandaBtemp(Max-Scartati), e_BandaBtemp(Max-Scartati))
+        
+        J = 1
+        Z = 1
+        print *, "Valori scartati : ", Scartati
+        !print *,"NumRiga, NumArray, NumArraytemp "
+        do while(J<=Max)
+          if (.not.((BandaB(J) >= 99.0).and.(BandaB(J)<= 99.99))) then
+            MJDtemp(Z) = MJD(J)
+            BandaBtemp(Z) = BandaB(J)
+            e_BandaBtemp(Z) = e_BandaB(J)
+            !print *, I, J, Z
+            !print *, MJDtemp(Z), BandaBtemp(Z), e_BandaBtemp(Z)
+            J = J + 1
+            Z = Z + 1
+          else
+            !print *, "Vado avanti perche dato non valido"
+            J = J + 1
+          end if
+        end do
+        
+        print *,"STAMPO I DATI CORRETTI IN UN FILE DI PROVA"
+
+        open(N, file=FileNomiSupernovae(F))
+        print *, FileNomiSupernovae(F)
+        do I=1, Z-1
+          write(N, *) MJDtemp(I), BandaBtemp(I), e_BandaBtemp(I)
+        end do
+        close(N)
+
         deallocate(MJD, BandaB, e_BandaB)
-        
-        
-        
+        deallocate(MJDtemp, BandaBtemp, e_BandaBtemp)
+
       end do
+
+
+      deallocate(PathSupernovae, FileNomiSupernovae)
+
       print *, "Fine del programma"
       
 
